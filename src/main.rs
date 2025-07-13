13a72951f8ba37f64e16663d0869e08fd69a834e
wh3pp <wh3pp@proton.me>
2025-07-13 04:38:06 -0500

    chore(deps): update dependencies and remove library Cargo.lock

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,25 @@
-use tunecore::{creators::SortBy, Error, TunecoreClient};
+mod db;
+mod ingestion;
+use db::{Db, DbResult};
+use dotenvy::dotenv;
+use std::env;
+use tunecore::{creators::SortBy, TunecoreClient};
 
 #[tokio::main]
-async fn main() -> Result<(), Error> {
+async fn main() -> DbResult<()> {
+    dotenv().ok();
+    let db_uri = env::var("DATABASE_URI").expect("DATABASE_URI must be set in .env file");
+    let db_name = env::var("DATABASE_NAME").expect("DATABASE_NAME must be set in .env file");
+
+    let db = Db::connect(&db_uri, &db_name).await?;
+    let songs_repo = db.songs();
+
     let client = TunecoreClient::new();
 
     let response = client
         .creators()
         .songs()
-        .per_page(10)
+        .per_page(20)
         .sort(SortBy::Popularity)
         .send()
         .await?;
@@ -16,6 +28,7 @@ async fn main() -> Result<(), Error> {
         println!("No response were found.");
     } else {
         println!("Found {} songs", response.community_songs.len());
+        songs_repo.save_many(&response.community_songs).await?;
     }
     Ok(())
 }
